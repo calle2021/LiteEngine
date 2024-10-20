284ddc8afe2e1d19939a97b529ab1935bc22dfdb
calle2021 <calle.janlenstenberg@gmail.com>
2024-10-20 13:33:02 +0200

    Macro name change to launch

--- PIXL/src/PIXL/EntryPoint.h
+++ PIXL/src/PIXL/EntryPoint.h
@@ -1,14 +1,14 @@
 #pragma once
 
 #ifdef _WIN32
-#define CREATE_APPLICATION( game ) \
+#define LAUNCH( game ) \
     int WINAPI wWinMain(_In_ HINSTANCE hins, _In_opt_ HINSTANCE, _In_ LPWSTR, _In_ int showcmd) \
     { \
         PIXL::Application::Init({hins, showcmd}); \
         return PIXL::Application::Run(game()); \
     }
 #else
-#define CREATE_APPLICATION( game ) \
+#define LAUNCH( game ) \
     int main(int argc, char *argv[]) \
     { \
         PIXL::Application::Init();
--- Sample/src/Main.cpp
+++ Sample/src/Main.cpp
@@ -7,5 +7,5 @@ public:
     }
 };
 
-CREATE_APPLICATION(Game);
+LAUNCH(Game);
 
