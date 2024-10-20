bc812ae2cc453508723e65f46acdb5ac89f7222e
calle2021 <calle.janlenstenberg@gmail.com>
2024-10-20 13:30:53 +0200

    Typo

--- PIXL/src/PIXL/EntryPoint.h
+++ PIXL/src/PIXL/EntryPoint.h
@@ -12,6 +12,6 @@
     int main(int argc, char *argv[]) \
     { \
         PIXL::Application::Init();
-        return PIXL::Application::Run(game(), {hins, showcmd}); \
+        return PIXL::Application::Run(game()); \
     }
 #endif
