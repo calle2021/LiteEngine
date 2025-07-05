9ad6abd1a43e3c682bac22a7412cb21f4761feb7
calle2021 <calle.janlenstenberg@gmail.com>
2025-07-05 13:01:02 +0200

    Test renaming case sensitive folder

--- format.sh
+++ format.sh
@@ -1,3 +1,2 @@
-find $1 -type f \( -name "*.cpp" -o -name "*.h" \) -exec clang-format -i {} +
+find -type f \( -name "*.cpp" -o -name "*.h" \) -exec clang-format -i {} +
 
-echo "All .cpp and .h files formatted with clang-format style: $STYLE"
